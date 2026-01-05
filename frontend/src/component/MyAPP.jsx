8cbe9b1c53cc72c511a61db2b11a0bb4046affe2
jagritisharma1802 <jagritisharma1802@gmail.com>
2026-01-05 15:38:30 +0530

    connection of all the tools in home page

---
+++ frontend/src/component/MyAPP.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Species from "./components/Species";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Species />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
