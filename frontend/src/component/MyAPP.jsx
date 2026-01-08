ea01a3427e056c2890a0d146b2e70eca4ed3c596
jagritisharma1802 <jagritisharma1802@gmail.com>
2026-01-08 21:15:02 +0530

    under my app

--- frontend/src/component/MyAPP.jsx
+++ frontend/src/component/MyAPP.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./navbar";
 import Hero from "./hero";
 import Features from "./features";
-import HowItWork from "./howItWork";
+import HowItWork from "./work";
 import Contact from "./contact";
 import Species from "./species";
 
