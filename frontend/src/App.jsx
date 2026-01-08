6f402784d68425e87c731b617e9667bb3eb567f7
jagritisharma1802 <jagritisharma1802@gmail.com>
2026-01-08 19:17:02 +0530

    merging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import MYAPP from "./components/MyApp"
+import MyApp from "./pages/MyApp";
 import { Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
