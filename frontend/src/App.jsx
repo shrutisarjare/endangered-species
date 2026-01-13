924a698a5947d16f920636db2ca38975a8cdedf3
jagritisharma1802 <jagritisharma1802@gmail.com>
2026-01-13 11:41:48 +0530

    in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
-import MyAPP from "./component/MyAPP.jsx";
-import { Routes, Route } from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Dashboard from "./pages/Dashboard"
-import Region from "./pages/Region"
-import PanIndia from "./pages/PanIndia"
-import PanIndiaLand from "./pages/PanIndiaLand"
+
+import MyApp from "./component/MyAPP.jsx";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Region from "./pages/Region";
+import PanIndia from "./pages/PanIndia";
+import PanIndiaLand from "./pages/PanIndiaLand";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<MyApp />} />
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/region" element={<Region />} />
       <Route path="/pan-india" element={<PanIndia />} />
       <Route path="/pan-india/land" element={<PanIndiaLand />} />
-      <MyApp />
     </Routes>
-  )
+  );
 }
